8f2ac7f0a8d587b1cd9de119e2031361d4dcf2ea
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2023-01-23 22:14:49 +0545

    minor changes

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const Block blocks[] = {
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-tasks",	10,	26},
 	{"",	"sb-music",	0,	11},
-	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-news",		0,	6},
+	/*{"",	"sb-pacpackages",	0,	8},*/
+	/*{"",	"sb-news",		0,	6},*/
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
 	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
@@ -17,21 +17,22 @@ static const Block blocks[] = {
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-doppler",	0,	13},
-	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
+	/*{"",	"sb-doppler",	0,	13},*/
+	/*{"",	"sb-forecast",	18000,	5},*/
+	/*{"",	"sb-mailbox",	180,	12},*/
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	{" ",	"sb-clock",	60,	1},
+	/*{"",	"sb-internet",	5,	4},*/
+	/*{"",	"sb-help-icon",	0,	15},*/
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "  ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
